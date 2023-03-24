d5f235032ac9d65dcbaa11ebd2d6f45aba75e74e
efraitor_ <efraitor.contact@gmail.com>
2023-03-24 09:28:15 +0100

    Changes
    -Added change scene mechanics

---
+++ Assets/Scripts/LevelMechanics/ExitArea.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; //Librería para el cambio entre escenas
+
+public class ExitArea : MonoBehaviour
+{
+    //Nombre de la escena a cargar
+    public string sceneToLoad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
